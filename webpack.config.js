74e60d3a041a9896992b9b07323dfa8eba7b51ab
Nico Adams <dominic.adams@mark43.com>
2016-08-26 00:53:23 -0400

    Added test javascript files

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    entry: "./scripts/main.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
